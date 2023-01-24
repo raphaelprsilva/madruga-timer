6491422f844b3ca1f1f55591044730770c73ca21
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2023-01-24 13:17:42 -0300

    feat: add global styles

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,21 @@
+export const defaultTheme = {
+  white: '#FFFFFF',
+
+  'gray-100': '#E1E1E6',
+  'gray-300': '#C4C4CC',
+  'gray-400': '#8D8D99',
+  'gray-500': '#7C7C8A',
+  'gray-600': '#323238',
+  'gray-700': '#29292E',
+  'gray-800': '#202024',
+  'gray-900': '#121214',
+
+  'purple-300': '#B066F9',
+  'purple-500': '#9735F7',
+  'purple-700': '#7E09F0',
+
+  'red-500': '#AB222E',
+  'red-700': '#7A1921',
+
+  'yellow-500': '#FBA94C',
+};
