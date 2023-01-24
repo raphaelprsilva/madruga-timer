a959b3d0c97839d0d12e4cb78fb1fca870daf1d2
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2023-01-24 14:50:32 -0300

    feat: set app Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+
+import { Router } from './Router';
+
+import { defaultTheme } from './styles/themes/default';
+import { GlobalStyle } from './styles/global';
+
 export function App() {
   return (
-    <div>
-        Hello
-    </div>
-  )
+    <ThemeProvider theme={ defaultTheme }>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 }
 
